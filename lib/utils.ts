efbae1e1f93655ce9dca17679c2828a3d947e91f
Ilias Ism <me@il.ly>
2026-09-06 12:56:48 +0200

    Migrate from tailwind-merge and clsx to cn

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,2 @@
-import { type ClassValue, clsx } from 'clsx'
-import { twMerge } from 'tailwind-merge'
-
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
-}
+// biome-ignore lint/performance/noBarrelFile: official shadcn cn re-export
+export { cn } from 'cn'
